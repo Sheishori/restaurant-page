3004b502caab1f3a6b8231857b449e2d585f392a
Sheishori <sheishori@gmail.com>
2022-06-08 13:43:52 +0200

    Enable tab switching

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import { pageLoad } from "./body";
 import { homeTab } from "./home";
+import { menuTab } from "./menu";
+import { contactTab } from "./contact";
+import { deliveryTab } from "./delivery";
 
 pageLoad();
 homeTab();
+
+const info = document.querySelector(".info");
+const menuTabs = document.querySelectorAll("li");
+console.log(menuTabs);
+menuTabs.forEach(tab => {
+	tab.addEventListener("click", () => {
+		const tabName = tab.textContent;
+		info.textContent = "";
+		if (tabName === "Home") homeTab();
+		if (tabName === "Menu") menuTab();
+		if (tabName === "Contact") contactTab();
+		if (tabName === "Delivery") deliveryTab();
+	});
+});
