3de44b9093b0e05b701e6196a7e7ad05dc8e6e9e
Sheishori <sheishori@gmail.com>
2022-06-08 15:18:04 +0200

    Wrap generated tab content to avoid overlapping CSS

--- src/home.js
+++ src/home.js
@@ -1,13 +1,16 @@
 function homeTab() {
+	const home = document.createElement("div");
 	const image = document.createElement("div");
 	const text = document.createElement("div");
 
+	home.setAttribute("id", "home");
 	image.classList.add("food-image");
 	text.classList.add("text");
 	text.textContent = "Saiou Sushi specializes in delicious food featuring fresh ingredients and masterful preparation by our culinary team. Whether you're ordering a multi-course meal or grabbing a drink and a bowl of ramen, Saikou Sushi's lively, casual yet upscale atmosphere makes it perfect for dining with friends, family, clients and business associates.";
 
+	home.append(image, text);
 	const info = document.querySelector(".info");
-	info.append(image, text);
+	info.appendChild(home);
 };
 
 export { homeTab };
