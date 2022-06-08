ce5a22d516fd426c408ccecafe086f7801279054
Sheishori <sheishori@gmail.com>
2022-06-08 15:20:21 +0200

    Add contents of the Delivery page

--- src/delivery.js
+++ src/delivery.js
@@ -1,7 +1,20 @@
 function deliveryTab() {
 	const delivery = document.createElement("div");
-	delivery.textContent = "delivery";
+	const img = document.createElement("img");
+	const order = document.createElement("div");
+	const list = document.createElement("ul");
+	const phone = document.createElement("li");
+	const uber = document.createElement("li");
 
+	delivery.setAttribute("id", "delivery");
+	img.src = "./img/truck-fast.png";
+	img.alt = "fast truck image"
+	order.textContent = "Order with delivery by:";
+	phone.textContent = "Phone: +0 123 456 789";
+	uber.innerHTML = "Uber Eats <button>Order Now</button>";
+
+	list.append(phone, uber);
+	delivery.append(img, order, list);
 	const info = document.querySelector(".info");
 	info.append(delivery);
 };
